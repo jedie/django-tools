368e811d3fa218823a9e6cb00d3bf7b741794cdf
JensDiemer <git@jensdiemer.de>
2009-06-16 08:41:26 +0000

    add "SlowerDevServer": Simple slow down the django developer server. The middleware insert in every 200 response a time.sleep

---
+++ django_tools/middlewares/SlowerDevServer.py
@@ -0,0 +1,34 @@
+# coding:utf-8
+
+"""
+    SlowerDevServer
+    ~~~~~~~~~~~~~~~
+    
+    Simple slow down the django developer server.
+    The middleware insert in every 200 response a time.sleep
+    
+    Put this into your settings:   
+    --------------------------------------------------------------------------
+        MIDDLEWARE_CLASSES = (
+            ...
+            'django_tools.middlewares.SlowerDevServer.SlowerDevServerMiddleware',
+            ...
+        )
+        SLOWER_DEV_SERVER_SLEEP = 0.3 # time.sleep() value (in sec.)
+    --------------------------------------------------------------------------
+"""
+
+import time
+import warnings
+
+from django.conf import settings
+
+class SlowerDevServerMiddleware(object):
+    def __init__(self):
+        warnings.warn("Slower developer server used!")
+        
+    def process_response(self, request, response):
+        if response.status_code == 200: 
+            print "SlowerDevServerMiddleware: Wait for %sSec..." % settings.SLOWER_DEV_SERVER_SLEEP
+            time.sleep(settings.SLOWER_DEV_SERVER_SLEEP)
+        return response
