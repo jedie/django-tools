3c40150fd4d64a50a17c292ba4756a54f98164d4
JensDiemer <git@jensdiemer.de>
2011-05-17 15:00:37 +0200

    for tests

---
+++ django_tools/tests/models.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from django.forms.models import ModelForm
+from django_tools.fields.sign_separated import SignSeparatedModelField
+
+
+
---
+++ django_tools/tests/test_settings.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ":memory:"
+    }
+}
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+
+    'django_tools',
+    'django_tools.tests',
+)
+SITE_ID = 1
