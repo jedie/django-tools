c0cc26941f5fd17801434f677fddab671a974933
Kwaw Annor <kwawannor@gmail.com>
2014-04-13 18:21:50 +0200

    Fixed usermodel import issue

--- django_tools/models.py
+++ django_tools/models.py
@@ -10,7 +10,7 @@
 
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model; User = get_user_model()
 try:
     from django.utils.timezone import now
 except ImportError:
