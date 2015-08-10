875903ee7333cac00d51a605c593ed2706029415
JensDiemer <git@jensdiemer.de>
2015-08-10 15:52:31 +0200

    Skip test in django 1.6

--- tests/test_installed_apps_utils.py
+++ tests/test_installed_apps_utils.py
@@ -1,10 +1,28 @@
+from functools import wraps
+import unittest
 
 from django.test import SimpleTestCase
-from django.test.utils import modify_settings
+
+
+class _Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+    def __call__(self, test_func):
+        @wraps(test_func)
+        def inner(*args, **kwargs):
+            return test_func(*args, **kwargs)
+        return inner
+
+try:
+    from django.test.utils import modify_settings
+except ImportError:
+    # modify_settings is new in Django 1.7
+    modify_settings = _Mock
 
 from django_tools.utils.installed_apps_utils import get_filtered_apps
 
 
+@unittest.skipIf(isinstance(modify_settings, _Mock), "SKIP: modify_settings is not available :(")
 @modify_settings(INSTALLED_APPS={
     'prepend': [
         'django.contrib.admindocs',
