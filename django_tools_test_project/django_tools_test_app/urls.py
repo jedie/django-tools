bbfcfc4b255dbe84ce0008b11e43eec439439998
JensDiemer <git@jensdiemer.de>
2016-08-26 22:39:53 +0200

    fix unittests for django v1.10

--- django_tools_test_project/django_tools_test_app/urls.py
+++ django_tools_test_project/django_tools_test_app/urls.py
@@ -14,10 +14,10 @@ from __future__ import absolute_import, division, print_function
 from django_tools_test_project.django_tools_test_app.views import display_site
 from django_tools_test_project.django_tools_test_app.views import get_current_get_parameters
 from django_tools_test_project.django_tools_test_app.views import raise_exception
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = (
     url(r'^display_site/$', display_site),
     url(r'^get_current_get_parameters/$', get_current_get_parameters),
     url(r'^raise_exception/$', raise_exception),
