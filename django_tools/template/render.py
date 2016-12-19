5dee3e03fb4350e66f1aa5e506abdad6d099e1ab
JensDiemer <git@jensdiemer.de>
2016-12-19 15:15:06 +0100

    NEW: django_tools.template.render.render_template_file

--- django_tools/template/render.py
+++ django_tools/template/render.py
@@ -6,13 +6,24 @@
 
     Some tools around template rendering
 
-    :copyleft: 2009-2015 by the django-tools team, see AUTHORS for more details.
+    :copyleft: 2009-2016 by the django-tools team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
 from __future__ import absolute_import, division, print_function
 
 from django.template import Context, Template
+from django.template.loader import get_template
+
+
+def render_template_file(path, context):
+    """
+    Render given template file and return the rendered string
+    """
+    t = get_template(path)
+    context = Context(context)
+    return t.render(context)
+
 
 def render_string_template(template, context):
     """
--- django_tools_tests/test_render.py
+++ django_tools_tests/test_render.py
@@ -5,12 +5,20 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
+
 from django.test import SimpleTestCase
 
-from django_tools.template.render import render_string_template
+from django_tools.template.render import render_string_template, \
+    render_template_file
 
 
 class TestRender(SimpleTestCase):
+    def test_render_template_file(self):
+        context={"foo":"bar"}
+        path = "test_template.html"
+        x = render_template_file(path, context)
+        self.assertEqual(x, "Hello bar !\n")
+
     def test_render_string_template(self):
         x = render_string_template("Foo {{ bar }}!", {"bar": "BAR"})
         self.assertEqual(x, 'Foo BAR!')
