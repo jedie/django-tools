6990aadb0e5ee78613dbdc90cb994e4ad859cf65
JensDiemer <git@jensdiemer.de>
2017-01-25 22:04:44 +0100

    django-filer v1.2.6 was released

--- django_tools_tests/test_mockup.py
+++ django_tools_tests/test_mockup.py
@@ -5,35 +5,13 @@
     ~~~~~~~~~~~~
 """
 
-from __future__ import unicode_literals, print_function
+from __future__ import unicode_literals, print_function, absolute_import
 
-import unittest
+import django
 
-from django_tools.unittest_utils.mockup import create_pil_image, create_info_image
+from django_tools.unittest_utils.mockup import create_pil_image, create_info_image, \
+    create_temp_filer_info_image
 from django_tools.unittest_utils.unittest_base import BaseTestCase
-
-
-#==============================================================================
-# TODO: Remove after django-filer v1.2.6 is released!
-# Problem: AttributeError: 'Manager' object has no attribute '_inherited'
-# with Django v1.10 and django-filer v1.2.5
-# see also:
-# https://github.com/divio/django-filer/issues/899
-
-from pip._vendor.packaging.version import parse as _parse_version
-from filer import __version__ as _filer_version
-from django import __version__ as _django_version
-
-_filer_version=_parse_version(_filer_version)
-_django_version=_parse_version(_django_version)
-
-if _django_version < _parse_version("1.10") or _filer_version >= _parse_version("1.2.6"):
-    SKIP_TEST = False
-    from django_tools.unittest_utils.mockup import create_temp_filer_info_image
-else:
-    SKIP_TEST = True
-
-#==============================================================================
 
 
 class TestMockup(BaseTestCase):
@@ -53,7 +31,6 @@ class TestMockup(BaseTestCase):
 
         self.assertEqual(pil_image.verify(), None)
 
-    @unittest.skipIf(SKIP_TEST, "Combination Django v1.10 and django-filer v1.2.5")
     def test_create_temp_filer_info_image(self):
         self.create_testusers(verbosity=1)
 
@@ -64,5 +41,11 @@ class TestMockup(BaseTestCase):
         self.assertEqual(filer_info_image.height, 300)
 
         self.assertEqual(filer_info_image.size, 1791)
-        self.assertIn("django-tools/filer_public/", filer_info_image.path)
         self.assertIn(".png", filer_info_image.path)
+
+        if django.VERSION < (1, 10):
+            self.assertIn("django-tools/filer_public/", filer_info_image.path)
+            #--------------------^
+        else:
+            self.assertIn("django_tools/filer_public/", filer_info_image.path)
+            #--------------------^
