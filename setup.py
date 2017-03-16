f728f236275e430be3727a30f5a42c9dcb0d46ed
JensDiemer <git@jensdiemer.de>
2017-03-16 17:07:04 +0100

    don't set max. version number

--- setup.py
+++ setup.py
@@ -207,7 +207,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,  # include package data under svn source control
     install_requires=[
-        "Django>=1.8,<1.11",
+        "Django>=1.8",
     ],
     zip_safe=False,
     classifiers=[
