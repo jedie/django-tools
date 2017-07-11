4a27b560948d8c43ac5be566b008a9c2974d5f08
JensDiemer <git@jensdiemer.de>
2017-07-11 10:32:17 +0200

    Update render_template_file() for Django v1.11

--- django_tools/template/render.py
+++ django_tools/template/render.py
@@ -6,7 +6,7 @@
 
     Some tools around template rendering
 
-    :copyleft: 2009-2016 by the django-tools team, see AUTHORS for more details.
+    :copyleft: 2009-2017 by the django-tools team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
@@ -21,7 +21,6 @@ def render_template_file(path, context):
     Render given template file and return the rendered string
     """
     t = get_template(path)
-    context = Context(context)
     return t.render(context)
 
 
