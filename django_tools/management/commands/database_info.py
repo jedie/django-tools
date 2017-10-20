8b1d1549b295a63be2b3d2b2e2c5de330eb7d0fe
JensDiemer <git@jensdiemer.de>
2017-10-20 11:18:51 +0200

    NEW: "database_info" manage command

---
+++ django_tools/management/commands/database_info.py
@@ -0,0 +1,68 @@
+# coding: utf-8
+
+"""
+    database_info manage command
+
+
+    setup:
+
+        INSTALLED_APPS = [
+            ...
+            'django_tools',
+            ...
+        ]
+
+
+    usage:
+
+        $ ./manage.py database_info
+
+
+    :copyleft: 2017 by the django-tools team, see AUTHORS for more details.
+    :created: 2017 by Jens Diemer
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+from pprint import pprint
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db import connections
+
+
+class Command(BaseCommand):
+    help = "Information about the used database and connections"
+
+    def handle(self, *args, **options):
+        self.stdout.write("")
+        self.stdout.write("_"*79)
+        self.stdout.write(self.help)
+        self.stdout.write("")
+
+        # self.stdout.write("settings.DATABASES = ", ending="")
+        # pprint(settings.DATABASES)
+        # self.stdout.write("-"*79)
+
+        for alias, settings_dict in settings.DATABASES.items():
+            self.stdout.write("Database alias %r:" % alias)
+            self.stdout.write("")
+            engine = settings_dict["ENGINE"]
+            engine = engine.rsplit(".",1)[-1]
+            self.stdout.write("engine..: %r" % engine)
+            self.stdout.write("name....: %r" % settings_dict["NAME"])
+            self.stdout.write("user....: %r" % settings_dict["USER"])
+            self.stdout.write("host....: %r" % settings_dict["HOST"])
+            self.stdout.write("port....: %r" % settings_dict["PORT"])
+
+        connection_list = connections.all()
+
+        self.stdout.write("")
+        self.stdout.write("There are %i connections." % len(connection_list))
+
+        for no, conn in enumerate(connection_list, 1):
+            self.stdout.write("")
+            self.stdout.write("connection %i alias: %r settings_dict:" % (no, conn.alias))
+            pprint(conn.settings_dict)
+
+        self.stdout.write("")
+        self.stdout.write("-"*79)
---
+++ django_tools_tests/test_database_info.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+
+"""
+    Test /django_tools/management/commands/database_info.py
+
+    :copyleft: 2017 by the django-tools team, see AUTHORS for more details.
+    :created: 2017 by Jens Diemer
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+from __future__ import print_function, unicode_literals
+
+import os
+from unittest import TestCase
+
+from django.core.management import call_command
+from django.test import SimpleTestCase
+
+# https://github.com/jedie/django-tools
+import django_tools
+from django_tools.unittest_utils.django_command import DjangoCommandMixin
+from django_tools.unittest_utils.stdout_redirect import StdoutStderrBuffer
+
+MANAGE_DIR = os.path.abspath(os.path.join(os.path.dirname(django_tools.__file__), ".."))
+
+
+class DatabaseInfoCallCommandTests(SimpleTestCase):
+    """
+    Test directly via django.core.management.call_command()
+    Note:
+        So the test database settings is active.
+    """
+    def test_database_info(self):
+        with StdoutStderrBuffer() as buff:
+            call_command("database_info")
+        output = buff.get_output()
+        print(output)
+
+        self.assertIn("engine..: 'sqlite3'", output)
+        self.assertIn("name....: 'file:memorydb_default?mode=memory&cache=shared'", output)
+
+        self.assertIn("There are 1 connections.", output)
+
+        self.assertIn("'CONN_MAX_AGE': 0,", output)
+
+
+class DatabaseInfoSubprocessTests(DjangoCommandMixin, TestCase):
+    """
+    Test command via "manage.py" subprocess
+    Note:
+        So the real database settings is active and not the test settings!
+    """
+    def test_database_info(self):
+        output = self.call_manage_py(["database_info"], manage_dir=MANAGE_DIR)
+        print(output)
+        self.assertIn("engine..: 'sqlite3'", output)
+        self.assertIn("name....: ':memory:'", output)
+
+        self.assertIn("There are 1 connections.", output)
+
+        self.assertIn("'CONN_MAX_AGE': 0,", output)
