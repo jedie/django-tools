aa9316f817ed737a2ec8f800a7d0a4aa7c3bb5ff
JensDiemer <git@jensdiemer.de>
2017-12-05 12:13:15 +0100

    rename InternalIps to FnMatchIps and make it useable for ALLOWED_HOSTS, too.

--- django_tools/settings_utils.py
+++ django_tools/settings_utils.py
@@ -4,7 +4,7 @@
     utilities for settings.py
     ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    :copyleft: 2015 by the django-tools team, see AUTHORS for more details.
+    :copyleft: 2015-2017 by the django-tools team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
@@ -14,22 +14,29 @@ from __future__ import absolute_import, division, print_function
 from fnmatch import fnmatch
 
 
-class InternalIps(list):
+class FnMatchIps(list):
     """
-    Allows you to use Unix shell-style wildcards of IP addresses in your INTERNAL_IPS.
-    Used the fnmatch module.
+    Allows you to use Unix shell-style wildcards of IP addresses
+    for settings.INTERNAL_IPS and settings.ALLOWED_HOSTS
+    used the fnmatch module.
 
     settings.py e.g.:
     --------------------------------------------------------------------------
-    from django_tools.settings_utils import InternalIps
+    from django_tools.settings_utils import FnMatchIps
 
-    INTERNAL_IPS = InternalIps(["127.0.0.1", "::1", "192.168.*.*", "10.0.*.*"])
+    INTERNAL_IPS = FnMatchIps(["127.0.0.1", "::1", "192.168.*.*", "10.0.*.*"])
+    ALLOWED_HOSTS = FnMatchIps(["127.0.0.1", "::1", "192.168.*.*", "10.0.*.*"])
     --------------------------------------------------------------------------
 
     borrowed from https://djangosnippets.org/snippets/1380/
     """
-    def __contains__(self, key):
+    def _fnmatch(self, pat):
         for ip in self:
-            if fnmatch(key, ip): return True
+            if fnmatch(pat, ip): return True
         return False
 
+    __eq__ = _fnmatch # ALLOWED_HOSTS compares, see: django.http.request.validate_host
+    __contains__ = _fnmatch # INTERNAL_IPS
+
+
+InternalIps = FnMatchIps # for compatibility
