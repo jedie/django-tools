fd282ade8c41868a1c6705bcabd5b9f07bd0eb47
JensDiemer <git@jensdiemer.de>
2017-12-19 14:24:02 +0100

    +test_get_or_create_user_and_group()

--- django_tools_tests/test_unittest_utils_user.py
+++ django_tools_tests/test_unittest_utils_user.py
@@ -12,11 +12,16 @@ import pytest
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sessions.models import Session
+from django.core.management import call_command
 from django.test import TestCase
 
 # https://github.com/jedie/django-tools
+from django_tools.permissions import get_filtered_permissions
+from django_tools.unittest_utils.stdout_redirect import StdoutStderrBuffer
 from django_tools.unittest_utils.unittest_base import BaseTestCase
-from django_tools.unittest_utils.user import create_user, get_super_user, TestUserMixin
+from django_tools.unittest_utils.user import TestUserMixin, create_user, get_or_create_user_and_group, get_super_user
 
 
 class TestUserUtils(TestUserMixin, BaseTestCase):
@@ -72,6 +77,7 @@ class TestUserUtils(TestUserMixin, BaseTestCase):
         self.assertTrue(user3.is_superuser)
 
 
+
 class TestUserFixtures(TestUserMixin, BaseTestCase):
     def assert_user_fixtures(self):
         users = self.UserModel.objects.all()
@@ -83,3 +89,107 @@ class TestUserFixtures(TestUserMixin, BaseTestCase):
         self.assert_user_fixtures()
         self.create_testusers() # create user a second time
         self.assert_user_fixtures()
+
+    def test_get_or_create_user_and_group(self):
+        superuser = self.UserModel.objects.filter(is_superuser=True, is_active=True)[0]
+        encrypted_password = superuser.password
+
+        test_user = get_or_create_user_and_group(
+            username="testuser",
+            groupname="testgroup",
+            permissions=get_filtered_permissions(
+                exclude_app_labels=("admin", "sites"),
+                exclude_models=(
+                    Session,
+                ),
+                exclude_codenames=(
+                    "delete_user",
+                    "delete_group",
+                ),
+                exclude_permissions=(
+                    (ContentType, "add_contenttype"),
+                    (ContentType, "delete_contenttype"),
+                )
+            ),
+            encrypted_password=encrypted_password
+        )
+
+        with StdoutStderrBuffer() as buff:
+            call_command("permission_info", "testuser")
+        output = buff.get_output()
+
+        output = [line.strip(" \t_") for line in output.splitlines()]
+        output = "\n".join([line for line in output if line])
+        print(output)
+
+        self.assertEqual_dedent(output, """
+            Display effective user permissions in the same format as user.has_perm() argument: <appname>.<codename>
+            All permissions for user 'testuser':
+            is_active    : yes
+            is_staff     : yes
+            is_superuser : no
+            [ ] admin.add_logentry
+            [ ] admin.change_logentry
+            [ ] admin.delete_logentry
+            [*] auth.add_group
+            [*] auth.change_group
+            [ ] auth.delete_group
+            [*] auth.add_permission
+            [*] auth.change_permission
+            [*] auth.delete_permission
+            [*] auth.add_user
+            [*] auth.change_user
+            [ ] auth.delete_user
+            [ ] contenttypes.add_contenttype
+            [*] contenttypes.change_contenttype
+            [ ] contenttypes.delete_contenttype
+            [*] django_tools_test_app.add_limittousergroupstestmodel
+            [*] django_tools_test_app.change_limittousergroupstestmodel
+            [*] django_tools_test_app.delete_limittousergroupstestmodel
+            [*] django_tools_test_app.add_permissiontestmodel
+            [*] django_tools_test_app.change_permissiontestmodel
+            [*] django_tools_test_app.delete_permissiontestmodel
+            [*] django_tools_test_app.extra_permission
+            [*] dynamic_site.add_sitealias
+            [*] dynamic_site.change_sitealias
+            [*] dynamic_site.delete_sitealias
+            [*] easy_thumbnails.add_source
+            [*] easy_thumbnails.change_source
+            [*] easy_thumbnails.delete_source
+            [*] easy_thumbnails.add_thumbnail
+            [*] easy_thumbnails.change_thumbnail
+            [*] easy_thumbnails.delete_thumbnail
+            [*] easy_thumbnails.add_thumbnaildimensions
+            [*] easy_thumbnails.change_thumbnaildimensions
+            [*] easy_thumbnails.delete_thumbnaildimensions
+            [*] filer.add_clipboard
+            [*] filer.change_clipboard
+            [*] filer.delete_clipboard
+            [*] filer.add_clipboarditem
+            [*] filer.change_clipboarditem
+            [*] filer.delete_clipboarditem
+            [*] filer.add_file
+            [*] filer.change_file
+            [*] filer.delete_file
+            [*] filer.add_folder
+            [*] filer.can_use_directory_listing
+            [*] filer.change_folder
+            [*] filer.delete_folder
+            [*] filer.add_folderpermission
+            [*] filer.change_folderpermission
+            [*] filer.delete_folderpermission
+            [*] filer.add_image
+            [*] filer.change_image
+            [*] filer.delete_image
+            [*] filer.add_thumbnailoption
+            [*] filer.change_thumbnailoption
+            [*] filer.delete_thumbnailoption
+            [ ] sessions.add_session
+            [ ] sessions.change_session
+            [ ] sessions.delete_session
+            [ ] sites.add_site
+            [ ] sites.change_site
+            [ ] sites.delete_site
+        """)
+
+        self.assertEqual(test_user.password, encrypted_password)
