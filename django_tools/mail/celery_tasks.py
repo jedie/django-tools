b863fd8ee3f57c31cd7d0b0bf5da63fdd7142905
JensDiemer <git@jensdiemer.de>
2018-02-05 10:21:48 +0100

    use from celery import shared_task instead of djcelery_transactions

--- django_tools/mail/celery_tasks.py
+++ django_tools/mail/celery_tasks.py
@@ -4,7 +4,7 @@
     mail util
     ~~~~~~~~~
 
-    :copyleft: 2017 by the django-tools team, see AUTHORS for more details.
+    :copyleft: 2017-2018 by the django-tools team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
@@ -19,32 +19,31 @@ from django.core.mail import send_mail as do_send_mail
 log = logging.getLogger(__name__)
 
 
-# https://github.com/fellowshipofone/django-celery-transactions
 try:
-    from djcelery_transactions import task
+    from celery import shared_task
 except ImportError as err:
-    CELERY_TRANSACTIONS_IMPORT_ERROR = err
+    CELERY_IMPORT_ERROR = err
 
     from functools import wraps
 
-    def task(*task_args, **task_kwargs):
+    def shared_task(*task_args, **task_kwargs):
         def task_decorator(func):
             @wraps(func)
             def func_wrapper(*args, **kwargs):
                 log.error(
-                    "django-celery-transactions not available: %s",
-                    CELERY_TRANSACTIONS_IMPORT_ERROR
+                    "celery not available: %s",
+                    CELERY_IMPORT_ERROR
                 )
                 return func(*args, **kwargs)
             return func_wrapper
         return task_decorator
 
 
-@task(name='mail:send_mail')
+@shared_task(name='mail:send_mail')
 def send_mail(subject, message, from_email, recipient_list):
     do_send_mail(subject, message, from_email, recipient_list)
 
 
-@task(name=settings.SEND_MAIL_CELERY_TASK_NAME)
+@shared_task(name=settings.SEND_MAIL_CELERY_TASK_NAME)
 def send_mail_celery_task(msg):
     msg.send()
