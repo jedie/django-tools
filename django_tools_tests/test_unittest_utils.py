c3048675344f84b0032d370c964ea4052850b74d
JensDiemer <git@jensdiemer.de>
2018-08-28 15:52:12 +0200

    code style update

--- django_tools_tests/test_unittest_utils.py
+++ django_tools_tests/test_unittest_utils.py
@@ -1,9 +1,7 @@
-
 """
     :copyleft: 2017-2018 by the django-tools team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
-
 
 import os
 import sys
@@ -30,6 +28,7 @@ from django_tools.unittest_utils.user import TestUserMixin
 
 
 class TestBaseUnittestCase(BaseUnittestCase):
+
     def test_dedent(self):
         """
         Test for BaseUnittestCase._dedent()
@@ -79,12 +78,17 @@ class TestBaseUnittestCase(BaseUnittestCase):
         if PY2:
             err_msg = err_msg.replace("u'", "'")
         print("***\n%s\n***" % err_msg)
-        self.assertEqual(err_msg, self._dedent("""
+        self.assertEqual(
+            err_msg,
+            self._dedent(
+                """
             'foo bar' != 'foo X bar'
             - foo bar
             + foo X bar
             ?    ++
-        """))
+        """
+            )
+        )
 
     def test_assertIn_dedent(self):
         self.assertIn_dedent(member="foo", container="The foo is here!")
@@ -106,10 +110,7 @@ class TestBaseUnittestCase(BaseUnittestCase):
         try:
             self.assert_is_dir("foobar_dir")
         except AssertionError as err:
-            self.assertEqual(
-                six.text_type(err),
-                six.text_type('Directory "foobar_dir" doesn\'t exists!')
-            )
+            self.assertEqual(six.text_type(err), six.text_type('Directory "foobar_dir" doesn\'t exists!'))
 
     def test_assert_not_is_dir(self):
         self.assert_not_is_dir("foobar_dir")
@@ -118,30 +119,22 @@ class TestBaseUnittestCase(BaseUnittestCase):
             self.assert_not_is_dir(existing_path)
         except AssertionError as err:
             self.assertEqual(
-                six.text_type(err),
-                six.text_type('Directory "%s" exists, but should not exists!' % existing_path)
+                six.text_type(err), six.text_type('Directory "%s" exists, but should not exists!' % existing_path)
             )
-
 
     def test_assert_is_file(self):
         self.assert_is_file(__file__)
         try:
             self.assert_is_file("foobar_file.txt")
         except AssertionError as err:
-            self.assertEqual(
-                six.text_type(err),
-                six.text_type('File "foobar_file.txt" doesn\'t exists!')
-            )
+            self.assertEqual(six.text_type(err), six.text_type('File "foobar_file.txt" doesn\'t exists!'))
 
     def test_assert_not_is_File(self):
         self.assert_not_is_File("foobar_file.txt")
         try:
             self.assert_not_is_File(__file__)
         except AssertionError as err:
-            self.assertEqual(
-                six.text_type(err),
-                six.text_type('File "%s" exists, but should not exists!' % __file__)
-            )
+            self.assertEqual(six.text_type(err), six.text_type('File "%s" exists, but should not exists!' % __file__))
 
     def test_assert_startswith(self):
         self.assert_startswith("foobar", "foo")
@@ -166,7 +159,7 @@ class TestBaseUnittestCase(BaseUnittestCase):
             self.assertEqual(url, "/admin/django_tools_test_app/permissiontestmodel/%i/change/" % obj.pk)
 
     def test_get_admin_add_url(self):
-        url = self.get_admin_add_url(obj = PermissionTestModel)
+        url = self.get_admin_add_url(obj=PermissionTestModel)
         self.assertEqual(url, "/admin/django_tools_test_app/permissiontestmodel/add/")
 
 
@@ -188,16 +181,19 @@ class TestTempDir(BaseUnittestCase):
 
 
 class TestStdoutStderrBuffer(BaseUnittestCase):
+
     def test_text_type(self):
         with StdoutStderrBuffer() as buffer:
             print(six.text_type("print text_type"))
             sys.stdout.write(six.text_type("stdout.write text_type\n"))
             sys.stderr.write(six.text_type("stderr.write text_type"))
-        self.assertEqual_dedent(buffer.get_output(), """
+        self.assertEqual_dedent(
+            buffer.get_output(), """
             print text_type
             stdout.write text_type
             stderr.write text_type
-        """)
+        """
+        )
 
     def test_binary_type(self):
         if six.PY2:
@@ -205,27 +201,32 @@ class TestStdoutStderrBuffer(BaseUnittestCase):
                 print("print str")
                 sys.stdout.write("stdout.write str\n")
                 sys.stderr.write("stderr.write str")
-            self.assertEqual_dedent(buffer.get_output(), """
+            self.assertEqual_dedent(
+                buffer.get_output(), """
                 print str
                 stdout.write str
                 stderr.write str
-            """)
+            """
+            )
         elif six.PY3:
             # The print function will use repr
             with StdoutStderrBuffer() as buffer:
                 print(b"print binary_type")
                 sys.stdout.write(b"stdout.write binary_type\n")
                 sys.stderr.write(b"stderr.write binary_type")
-            self.assertEqual_dedent(buffer.get_output(), """
+            self.assertEqual_dedent(
+                buffer.get_output(), """
                 b'print binary_type'
                 stdout.write binary_type
                 stderr.write binary_type
-            """)
+            """
+            )
         else:
             self.fail()
 
 
 class TestBaseTestCase(TestUserMixin, BaseTestCase):
+
     def test_print_sql(self):
         with StdoutStderrBuffer() as buffer:
             with PrintQueries("Create object"):
@@ -258,6 +259,7 @@ class TestBaseTestCase(TestUserMixin, BaseTestCase):
 
 @set_string_if_invalid()
 class TestSetStringIfInvalidDecorator(SimpleTestCase):
+
     def assert_activation(self):
         from django.conf import settings
         string_if_invalid = settings.TEMPLATES[0]['OPTIONS']['string_if_invalid']
@@ -319,13 +321,15 @@ class TestCeleryDecoratorClassUsage(SimpleTestCase):
 
 
 class AssertResponseTest(BaseTestCase):
+
     @classmethod
     def setUpClass(cls):
         super(AssertResponseTest, cls).setUpClass()
         cls.response = Client().get("/admin/login/")
 
     def test_assert_response_ok(self):
-        self.assertResponse(self.response,
+        self.assertResponse(
+            self.response,
             must_contain=(
                 "Django administration",
                 "Username:",
@@ -333,7 +337,8 @@ class AssertResponseTest(BaseTestCase):
                 'value="Log in"',
             ),
             must_not_contain=(
-                "error", "traceback",
+                "error",
+                "traceback",
             ),
             status_code=200,
             template_name="admin/login.html",
@@ -342,33 +347,37 @@ class AssertResponseTest(BaseTestCase):
         )
 
     def test_must_contain(self):
-        self.assertResponse(self.response,
-            must_contain=("Django administration",)
-        )
-        self.assertRaises(AssertionError, self.assertResponse, self.response,
-            must_contain=("Django X administration",), browser_traceback=False
+        self.assertResponse(self.response, must_contain=("Django administration",))
+        self.assertRaises(
+            AssertionError,
+            self.assertResponse,
+            self.response,
+            must_contain=("Django X administration",),
+            browser_traceback=False
         )
 
     def test_must_not_contain(self):
-        self.assertRaises(AssertionError, self.assertResponse, self.response,
-            must_not_contain=("Django administration",), browser_traceback=False
+        self.assertRaises(
+            AssertionError,
+            self.assertResponse,
+            self.response,
+            must_not_contain=("Django administration",),
+            browser_traceback=False
         )
 
     def test_wrong_template(self):
-        self.assertResponse(self.response,
-            template_name="admin/login.html"
-        )
-        self.assertRaises(AssertionError, self.assertResponse, self.response,
-            template_name="admin/loginXXX.html", browser_traceback=False
+        self.assertResponse(self.response, template_name="admin/login.html")
+        self.assertRaises(
+            AssertionError,
+            self.assertResponse,
+            self.response,
+            template_name="admin/loginXXX.html",
+            browser_traceback=False
         )
 
     def test_wrong_status_code(self):
-        self.assertResponse(self.response,
-            status_code=200
-        )
-        self.assertRaises(AssertionError, self.assertResponse, self.response,
-            status_code=404, browser_traceback=False
-        )
+        self.assertResponse(self.response, status_code=200)
+        self.assertRaises(AssertionError, self.assertResponse, self.response, status_code=404, browser_traceback=False)
 
     def test_get_messages_normal_response(self):
         response = self.client.get("/create_message_normal_response/normal-response-message/")
@@ -377,11 +386,12 @@ class AssertResponseTest(BaseTestCase):
 
         self.assertMessages(response, ['normal-response-message'])
 
-        self.assertResponse(response,
+        self.assertResponse(
+            response,
             must_contain=("django_tools_test_app.views.create_message_normal_response",),
             must_not_contain=("error", "traceback"),
             status_code=200,
-            messages = ['normal-response-message'],
+            messages=['normal-response-message'],
             html=False,
             browser_traceback=True
         )
@@ -393,9 +403,10 @@ class AssertResponseTest(BaseTestCase):
 
         self.assertMessages(response, ['redirect-response-message'])
 
-        self.assertResponse(response,
+        self.assertResponse(
+            response,
             status_code=302,
-            messages = ['redirect-response-message'],
+            messages=['redirect-response-message'],
         )
 
     def test_wrong_messages(self):
@@ -403,21 +414,23 @@ class AssertResponseTest(BaseTestCase):
         self.assertMessages(response, ['this-is-it'])
 
         with self.assertRaises(AssertionError) as cm:
-            self.assertResponse(response,
+            self.assertResponse(
+                response,
                 messages=['this-is-not-it'],
                 browser_traceback=False,
             )
 
-        self.assert_exception_startswith(cm,
-            "Lists differ: ['this-is-it'] != ['this-is-not-it']"
-        )
+        self.assert_exception_startswith(cm, "Lists differ: ['this-is-it'] != ['this-is-not-it']")
 
     def test_print_mailbox(self):
         self.assertEqual(len(mail.outbox), 0)
 
         ok = SendMail(
             template_base="mail_test.{ext}",
-            mail_context={"foo": "first", "bar": "second"},
+            mail_context={
+                "foo": "first",
+                "bar": "second"
+            },
             subject="test test_print_mailbox()",
             recipient_list="foo@bar.tld"
         ).send()
