6f492e210e5cc8e75ddd356d3a779d3a53002fa7
Jens Diemer <git@jensdiemer.de>
2019-02-21 09:40:41 +0100

    handle attachments/alternatives in unittest_utils.email.print_mailbox

--- django_tools/unittest_utils/email.py
+++ django_tools/unittest_utils/email.py
@@ -1,11 +1,13 @@
 
 """
     :created: 2018 by Jens Diemer
-    :copyleft: 2018 by the django-tools team, see AUTHORS for more details.
+    :copyleft: 2018-2019 by the django-tools team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
+from django.utils.text import Truncator
 
-def print_mailbox(outbox):
+
+def print_mailbox(outbox, max_length=120):
     """
     Usefull for debugging tests, e.g.:
 
@@ -13,22 +15,48 @@ def print_mailbox(outbox):
         print_mailbox(mail.outbox)
         self.assertEqual(len(mail.outbox), 2)
     """
+
+    def cutted_bytes(data, num=80):
+        return Truncator(repr(data)).chars(num=num)
+
+    print("There are %i mails", len(outbox))
     for no, mail_instance in enumerate(outbox, start=1):
-        print("_"*79)
+        print("_" * 79)
         print(" *** Mail No. %i: ***" % no)
-        for attr_name in dir(mail_instance):
-            if attr_name.startswith("_"):
-                continue
+
+        attr_names = [
+            attr_name for attr_name in dir(mail_instance) if not attr_name.startswith("_") and attr_name != "body"
+        ]
+        attr_names.sort()
+        attr_names.append("body")
+
+        for attr_name in attr_names:
             attr = getattr(mail_instance, attr_name, "--")
-            if callable(attr):
+
+            if callable(attr) or not isinstance(attr, (str, list, tuple, bool)):
                 continue
-            if not isinstance(attr, (str, bytes, list, tuple, bool)) and attr is not None:
-                continue
-            if attr_name == "body":
-                print("-"*79)
-                print("%20s:" % attr_name)
+
+            print("%20s:" % attr_name, end=" ", flush=True)
+
+            if attr_name == "alternatives":
+                for data in attr:
+                    print(cutted_bytes(data, num=max_length))
+
+            elif attr_name == "attachments":
+                print("\n")
+                for filename, data, mine_type in attr:
+                    line = " * %s (%s)" % (filename, mine_type)
+                    print(line, end=" ")
+                    print(cutted_bytes(data, num=(max_length - len(line))))
+                print()
+
+            elif attr_name == "body":
                 print(attr)
-                print("-"*79)
+                print("-" * max_length)
+
             else:
-                print("%20s: %s" % (attr_name, attr))
-        print("="*79)
+                if len(attr) > max_length:
+                    attr = cutted_bytes(data, num=max_length)
+                print(attr)
+
+        print("=" * 120)
