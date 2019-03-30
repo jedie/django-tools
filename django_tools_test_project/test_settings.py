75283ba57162aa44a29ceee29580b6695c2e396c
JensDiemer <git@jensdiemer.de>
2019-03-30 16:27:55 +0100

    only code formatting with black

--- django_tools_test_project/test_settings.py
+++ django_tools_test_project/test_settings.py
@@ -21,85 +21,79 @@ ALLOWED_HOSTS = ["*"]  # Allow any domain/subdomain
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, "..", "test_project_db.sqlite3"),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "..", "test_project_db.sqlite3"),
         # 'NAME': ":memory:"
     }
 }
 
-CACHES = {'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache', 'LOCATION': 'unique-snowflake'}}
+CACHES = {"default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache", "LOCATION": "unique-snowflake"}}
 
 MIDDLEWARE_CLASSES = (
     # https://github.com/jazzband/django-debug-toolbar/
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     # 'django_tools.dynamic_site.middleware.DynamicSiteMiddleware',
-    'django_tools.middlewares.ThreadLocal.ThreadLocalMiddleware',
-    'django_tools.middlewares.TracebackLogMiddleware.TracebackLogMiddleware',
+    "django_tools.middlewares.ThreadLocal.ThreadLocalMiddleware",
+    "django_tools.middlewares.TracebackLogMiddleware.TracebackLogMiddleware",
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.admin',
-    'django.contrib.staticfiles',
-    'django.contrib.admindocs',
-    'django.contrib.flatpages',
-    'easy_thumbnails',
-    'filer',  # for django_tools.unittest_utils.mockup
-
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.admin",
+    "django.contrib.staticfiles",
+    "django.contrib.admindocs",
+    "django.contrib.flatpages",
+    "easy_thumbnails",
+    "filer",  # for django_tools.unittest_utils.mockup
     # TODO:
     # # https://pypi.org/project/django-parler
     # 'parler',
     #
     # # https://pypi.org/project/django-ya-model-publisher/
     # 'publisher',
-    'django_tools',
-    'django_tools.local_sync_cache',
+    "django_tools",
+    "django_tools.local_sync_cache",
     # 'django_tools.dynamic_site',
-    'django_tools_test_project.django_tools_test_app',
+    "django_tools_test_project.django_tools_test_app",
 )
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, "templates/"),
-        ],
-        'OPTIONS': {
-            'loaders': [
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates/")],
+        "OPTIONS": {
+            "loaders": [
                 (
-                    "django_tools.template.loader.DebugCacheLoader", (
-                        'django.template.loaders.filesystem.Loader',
-                        'django.template.loaders.app_directories.Loader',
-                    )
-                ),
+                    "django_tools.template.loader.DebugCacheLoader",
+                    ("django.template.loaders.filesystem.Loader", "django.template.loaders.app_directories.Loader"),
+                )
             ],
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.template.context_processors.media',
-                'django.template.context_processors.csrf',
-                'django.template.context_processors.tz',
-                'django.template.context_processors.static',
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.template.context_processors.media",
+                "django.template.context_processors.csrf",
+                "django.template.context_processors.tz",
+                "django.template.context_processors.static",
             ],
         },
-    },
+    }
 ]
 
 # Internationalization
@@ -112,23 +106,10 @@ LANGUAGE_CODE = "en"
 # http://django-parler.readthedocs.org/en/latest/quickstart.html#configuration
 PARLER_LANGUAGES = {
     1: [
-        {
-            "name": _("German"),
-            "code": "de",
-            "fallbacks": [LANGUAGE_CODE],
-            "hide_untranslated": False,
-        },
-        {
-            "name": _("English"),
-            "code": "en",
-            "fallbacks": ["de"],
-            "hide_untranslated": False,
-        },
+        {"name": _("German"), "code": "de", "fallbacks": [LANGUAGE_CODE], "hide_untranslated": False},
+        {"name": _("English"), "code": "en", "fallbacks": ["de"], "hide_untranslated": False},
     ],
-    "default": { # all SITE_ID"s
-        "fallbacks": [LANGUAGE_CODE],
-        "redirect_on_fallback": False,
-    },
+    "default": {"fallbacks": [LANGUAGE_CODE], "redirect_on_fallback": False},  # all SITE_ID"s
 }
 
 # https://docs.djangoproject.com/en/1.8/ref/settings/#languages
@@ -143,34 +124,32 @@ LANGUAGE_DICT = dict(LANGUAGES)  # useful to get translated name by language cod
 # http://django-parler.readthedocs.org/en/latest/quickstart.html#configuration
 PARLER_DEFAULT_LANGUAGE_CODE = LANGUAGE_CODE
 
-#==============================================================================
+# ==============================================================================
 
-EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
-#==============================================================================
+# ==============================================================================
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-#==============================================================================
+# ==============================================================================
 
 SITE_ID = 1
 
 USE_DYNAMIC_SITE_MIDDLEWARE = True
 
-ROOT_URLCONF = 'django_tools_test_project.django_tools_test_app.urls'
+ROOT_URLCONF = "django_tools_test_project.django_tools_test_app.urls"
 
-PASSWORD_HASHERS = (  # Speedup tests
-    'django.contrib.auth.hashers.MD5PasswordHasher',
-)
+PASSWORD_HASHERS = ("django.contrib.auth.hashers.MD5PasswordHasher",)  # Speedup tests
 
-#_____________________________________________________________________________
+# _____________________________________________________________________________
 
 # Adds 'cut_path' attribute on log record. So '%(cut_path)s' can be used in log formatter.
 # django_tools.unittest_utils.logging_utils.CutPathnameLogRecordFactory
@@ -180,7 +159,7 @@ logging.setLogRecordFactory(CutPathnameLogRecordFactory(max_length=50))
 # django_tools.unittest_utils.logging_utils.FilterAndLogWarnings
 warnings.showwarning = FilterAndLogWarnings()
 
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 LOGGING = {
     "version": 1,
