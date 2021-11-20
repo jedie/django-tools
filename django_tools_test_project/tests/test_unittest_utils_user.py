01104a6a682738c7c4a2196f7de2621ab1b85a1c
JensDiemer <git@jensdiemer.de>
2021-11-20 21:16:05 +0100

    use snapshot tests

--- django_tools_test_project/tests/test_unittest_utils_user.py
+++ django_tools_test_project/tests/test_unittest_utils_user.py
@@ -4,8 +4,7 @@
     :copyleft: 2017-2019 by the django-tools team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
-
-
+from bx_py_utils.test_utils.snapshot import assert_text_snapshot
 from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sessions.models import Session
@@ -110,118 +109,13 @@ class TestUserFixtures(TestUserMixin, BaseTestCase):
             call_command("permission_info", "testuser")
         output = buff.get_output()
 
-        output = [line.strip(" \t_") for line in output.splitlines()]
-        output = "\n".join([line for line in output if line])
-        print(output)
+        assert 'Display effective user permissions' in output
+        assert 'is_active    : yes' in output
+        assert '[ ] admin.add_logentry' in output
+        assert '[*] auth.add_group' in output
+        assert '[ ] sites.view_site' in output
 
-        assert_equal_dedent(
-            output,
-            """
-                Display effective user permissions in the same format as user.has_perm() argument: <appname>.<codename>
-                All permissions for user 'testuser':
-                is_active    : yes
-                is_staff     : yes
-                is_superuser : no
-                [ ] admin.add_logentry
-                [ ] admin.change_logentry
-                [ ] admin.delete_logentry
-                [ ] admin.view_logentry
-                [*] auth.add_group
-                [*] auth.change_group
-                [ ] auth.delete_group
-                [*] auth.view_group
-                [*] auth.add_permission
-                [*] auth.change_permission
-                [*] auth.delete_permission
-                [*] auth.view_permission
-                [*] auth.add_user
-                [*] auth.change_user
-                [ ] auth.delete_user
-                [*] auth.view_user
-                [ ] contenttypes.add_contenttype
-                [*] contenttypes.change_contenttype
-                [ ] contenttypes.delete_contenttype
-                [*] contenttypes.view_contenttype
-                [*] django_tools_test_app.add_limittousergroupstestmodel
-                [*] django_tools_test_app.change_limittousergroupstestmodel
-                [*] django_tools_test_app.delete_limittousergroupstestmodel
-                [*] django_tools_test_app.view_limittousergroupstestmodel
-                [*] django_tools_test_app.add_overwritefilesystemstoragemodel
-                [*] django_tools_test_app.change_overwritefilesystemstoragemodel
-                [*] django_tools_test_app.delete_overwritefilesystemstoragemodel
-                [*] django_tools_test_app.view_overwritefilesystemstoragemodel
-                [*] django_tools_test_app.add_permissiontestmodel
-                [*] django_tools_test_app.change_permissiontestmodel
-                [*] django_tools_test_app.delete_permissiontestmodel
-                [*] django_tools_test_app.extra_permission
-                [*] django_tools_test_app.view_permissiontestmodel
-                [*] django_tools_test_app.add_simpleparlermodel
-                [*] django_tools_test_app.change_simpleparlermodel
-                [*] django_tools_test_app.delete_simpleparlermodel
-                [*] django_tools_test_app.view_simpleparlermodel
-                [*] django_tools_test_app.add_usermediafiles
-                [*] django_tools_test_app.change_usermediafiles
-                [*] django_tools_test_app.delete_usermediafiles
-                [*] django_tools_test_app.view_usermediafiles
-                [*] easy_thumbnails.add_source
-                [*] easy_thumbnails.change_source
-                [*] easy_thumbnails.delete_source
-                [*] easy_thumbnails.view_source
-                [*] easy_thumbnails.add_thumbnail
-                [*] easy_thumbnails.change_thumbnail
-                [*] easy_thumbnails.delete_thumbnail
-                [*] easy_thumbnails.view_thumbnail
-                [*] easy_thumbnails.add_thumbnaildimensions
-                [*] easy_thumbnails.change_thumbnaildimensions
-                [*] easy_thumbnails.delete_thumbnaildimensions
-                [*] easy_thumbnails.view_thumbnaildimensions
-                [*] filer.add_clipboard
-                [*] filer.change_clipboard
-                [*] filer.delete_clipboard
-                [*] filer.view_clipboard
-                [*] filer.add_clipboarditem
-                [*] filer.change_clipboarditem
-                [*] filer.delete_clipboarditem
-                [*] filer.view_clipboarditem
-                [*] filer.add_file
-                [*] filer.change_file
-                [*] filer.delete_file
-                [*] filer.view_file
-                [*] filer.add_folder
-                [*] filer.can_use_directory_listing
-                [*] filer.change_folder
-                [*] filer.delete_folder
-                [*] filer.view_folder
-                [*] filer.add_folderpermission
-                [*] filer.change_folderpermission
-                [*] filer.delete_folderpermission
-                [*] filer.view_folderpermission
-                [*] filer.add_image
-                [*] filer.change_image
-                [*] filer.delete_image
-                [*] filer.view_image
-                [*] filer.add_thumbnailoption
-                [*] filer.change_thumbnailoption
-                [*] filer.delete_thumbnailoption
-                [*] filer.view_thumbnailoption
-                [*] flatpages.add_flatpage
-                [*] flatpages.change_flatpage
-                [*] flatpages.delete_flatpage
-                [*] flatpages.view_flatpage
-                [*] serve_media_app.add_usermediatokenmodel
-                [*] serve_media_app.change_usermediatokenmodel
-                [*] serve_media_app.delete_usermediatokenmodel
-                [*] serve_media_app.view_usermediatokenmodel
-                [ ] sessions.add_session
-                [ ] sessions.change_session
-                [ ] sessions.delete_session
-                [ ] sessions.view_session
-                [ ] sites.add_site
-                [ ] sites.change_site
-                [ ] sites.delete_site
-                [ ] sites.view_site
-        """,
-        )
+        assert_text_snapshot(got=output)
 
         assert_pformat_equal(test_user.password, encrypted_password)
 
