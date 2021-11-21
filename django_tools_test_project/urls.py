aa5006c0ee64debfda53608ed86660103b06e945
JensDiemer <git@jensdiemer.de>
2021-11-21 13:57:55 +0100

    update project urls.py

--- django_tools_test_project/urls.py
+++ django_tools_test_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls import include, static, url
+from django.conf.urls import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from django_tools_test_project.django_tools_test_app.views import (
     TemplateDoesNotExists,
@@ -16,19 +16,18 @@ from django_tools_test_project.django_tools_test_app.views import (
 
 admin.autodiscover()
 
-
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
-    url(r'^display_site/$', display_site),
-    url(r'^get_current_get_parameters/$', get_current_get_parameters),
-    url(r'^raise_exception/(?P<msg>.*?)/$', raise_exception),
-    url(r'^raise_template_not_exists/$', TemplateDoesNotExists.as_view()),
+    path('display_site/', display_site),
+    path('get_current_get_parameters/', get_current_get_parameters),
+    path('raise_exception/<str:msg>/', raise_exception),
+    path('raise_template_not_exists/', TemplateDoesNotExists.as_view()),
 
-    url(r'^create_message_normal_response/(?P<msg>.*?)/$', create_message_normal_response),
-    url(r'^create_message_redirect_response/(?P<msg>.*?)/$', create_message_redirect_response),
+    path('create_message_normal_response/<str:msg>/', create_message_normal_response),
+    path('create_message_redirect_response/<str:msg>/', create_message_redirect_response),
 
-    url(r'^delay/$', delay_view),
+    path('delay/', delay_view),
 
     # Serve user files only for authorized users:
     path(settings.MEDIA_URL.lstrip('/'), include('django_tools.serve_media_app.urls')),
@@ -40,5 +39,5 @@ if settings.DEBUG:
 
     import debug_toolbar
     urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
