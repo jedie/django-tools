13b07ac5b9c7b51da45264096e7eb7bda3e5914b
JensDiemer <git@jensdiemer.de>
2022-08-18 09:46:23 +0200

    Fix manage.sh by set "local" settings

--- manage.sh
+++ manage.sh
@@ -2,4 +2,6 @@
 
 # Interact with manage.py from our test project
 
+export DJANGO_SETTINGS_MODULE=django_tools_test_project.settings.local
+
 exec poetry run python3 django_tools_test_project/manage.py "$@"
