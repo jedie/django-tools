9e19e64074eae36430673f714bdf13bfa8f1ad59
JensDiemer <git@jensdiemer.de>
2025-11-28 21:36:29 +0100

    Run dev server command: Call "collectstatic", too

--- django_tools/__init__.py
+++ django_tools/__init__.py
@@ -4,5 +4,5 @@
 """
 
 # See https://packaging.python.org/en/latest/specifications/version-specifiers/
-__version__ = '0.57.0'
+__version__ = '0.58.0'
 __author__ = 'Jens Diemer <django-tools@jensdiemer.de>'
